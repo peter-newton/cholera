9889666a345af54b67c5323daff0bf51ec3703ed
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-09-16 00:17:51 -1000

    Updates

---
+++ precache-manifest.efeb47cf71c0b05f76524ecf9c57d4d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e66a9de26c298576ac9f377f4502002",
+    "url": "/cholera/index.html"
+  },
+  {
+    "revision": "12f9e64b972d7c719b68",
+    "url": "/cholera/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b01288ce390b21d7c7a2",
+    "url": "/cholera/static/js/2.afe33b80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cholera/static/js/2.afe33b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f9e64b972d7c719b68",
+    "url": "/cholera/static/js/main.1e95cd7f.chunk.js"
+  },
+  {
+    "revision": "c6b99fdbfd4592c26606",
+    "url": "/cholera/static/js/runtime-main.f03dc3ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cholera/static/media/logo.5d5d9eef.svg"
+  }
+]);
