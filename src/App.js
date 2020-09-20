6c1028ef70fdb45860f1d97921fb906932ef5e4b
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-09-20 11:19:02 -1000

    Updating more Initializations

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
+
+      <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/17.embed"></iframe>
+
+      <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/9.embed"></iframe>
+
+      <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/13.embed"></iframe>
+
+      <iframe width="900" height="800" frameBorder="0" scrolling="no" src="/Total Fatalities.html"></iframe>
     </div>
   );
 }
