582baf4c4f841ec2ec700773463a267937434fc0
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-09-19 18:27:47 -1000

    Update

--- src/App.js
+++ src/App.js
@@ -11,11 +11,28 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <iframe width="900" height="800" frameBorder="0" scrolling="no"
-                src="//plotly.com/dashboard/pnewton:4/embed"></iframe>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+              integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+              crossOrigin=""/>
 
-        <iframe width="900" height="800" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/2.embed"></iframe>
-        
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+                integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+                crossOrigin=""></script>
+
+        <div id="mapid"></div>
+<script>
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+  '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1
+}).addTo(mymap);
+</script>
       </header>
     </div>
   );
