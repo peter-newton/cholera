2827dcf8d890e81729125a3cc85605844c58b18e
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-09-20 18:00:29 -1000

    Map and Charts

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -8,25 +8,31 @@ class MapView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentLocation: { lat: 52.52437, lng: 13.41053 },
-      zoom: 12,
+      currentLocation: { lat: 51.51342, lng: -0.13642 },
+      zoom: 17,
     }
   }
 
   render() {
     const { currentLocation, zoom } = this.state;
-
     return (
+        <container>
       <Map center={currentLocation} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-
         <Markers venues={data.venues}/>
       </Map>
+          
+          <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/17.embed"></iframe>
+
+          <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/9.embed"></iframe>
+
+          <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/13.embed"></iframe>
+
+        </container>
     );
   }
 }
-
 export default MapView;
