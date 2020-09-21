a16bb90cd9c9436ce84d47953d0fc27c2df861c9
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2020-09-20 16:14:59 -1000

    Revamp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import MapView from './components/MapView';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-
-      <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/17.embed"></iframe>
-
-      <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/9.embed"></iframe>
-
-      <iframe width="600" height="500" frameBorder="0" scrolling="no" src="//plotly.com/~pnewton/13.embed"></iframe>
-
-      <iframe width="900" height="800" frameBorder="0" scrolling="no" src="/Total Fatalities.html"></iframe>
+      <MapView/>
     </div>
   );
 }
